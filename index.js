324887bab6e98ad3b84ee559f7d11a109b771e2a
Milo <miiloe.ru>
2018-04-09 20:28:08 +0000

    web server working with 404 and 200 codes as well as home.html

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 var http = require("http");
+var fs = require("fs");
 http.createServer(function(req,res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('this is working');
+  var path = req.url.toLowerCase();
+  switch(path) {
+    case '/':
+    case '/home.html':
+    case '/home':
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      fs.createReadStream("public/home.html").pipe(res);
+      break;
+    case '/about':
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      fs.createReadStream("package.json").pipe(res);
+      break;
+    default:
+      res.writeHead(404, {'Content-Type': 'text/plain'});
+      res.end('Error 404: page does not exist');
+      break;
+    }
 }).listen(process.env.PORT || 3000);
