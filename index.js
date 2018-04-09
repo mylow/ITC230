18ea3ad93fa120f83bb1b375871b1c6b0ead88c7
Milo <miiloe.ru>
2018-04-09 00:12:56 +0000

    adding web server script

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-test 1
+var http = require("http");
+http.createServer(function(req,res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('this is working');
+}).listen(process.env.PORT || 3000);
